e1ed0d83f208e922e1f84768f18eea8c68fb52ac
Bob Paauwe <bpaauwe@yahoo.com>
2019-12-10 17:04:05 -0800

    Redesign
    Make use of new common parameter handling functions and log level
    control functions.  Switch to using new driver update function with
    UOM handling.

---
+++ nodes/darksky.py
@@ -0,0 +1,331 @@
+#!/usr/bin/env python3
+"""
+Polyglot v2 node server DarkSky weather data
+Copyright (C) 2019 Robert Paauwe
+"""
+
+try:
+    import polyinterface
+except ImportError:
+    import pgc_interface as polyinterface
+import sys
+import requests
+import json
+import node_funcs
+from nodes import darksky_daily
+from nodes import uom
+import write_profile
+
+LOGGER = polyinterface.LOGGER
+
+@node_funcs.add_functions_as_methods(node_funcs.functions)
+class Controller(polyinterface.Controller):
+    id = 'dsweather'
+    #id = 'controller'
+    hint = [0,0,0,0]
+    def __init__(self, polyglot):
+        super(Controller, self).__init__(polyglot)
+        self.name = 'DarkSky'
+        self.address = 'weather'
+        self.primary = self.address
+        self.configured = False
+        #self.location = ''
+        #self.apikey = ''
+        #self.units = 'us'
+        #self.myConfig = {}
+        #self.plant_type = 0.23
+        #self.elevation = 0
+
+        self.params = node_funcs.NSParameters([{
+            'name': 'APIKey',
+            'default': 'set me',
+            'isRequired': True,
+            'notice': 'DarkSky API key must be set',
+            },
+            {
+            'name': 'Location',
+            'default': '',
+            'isRequired': True,
+            'notice': 'DarkSky location must be set',
+            },
+            {
+            'name': 'Units',
+            'default': 'us',
+            'isRequired': False,
+            'notice': '',
+            },
+            {
+            'name': 'Forecast Days',
+            'default': '0',
+            'isRequired': False,
+            'notice': '',
+            },
+            {
+            'name': 'Elevation',
+            'default': '0',
+            'isRequired': False,
+            'notice': '',
+            },
+            {
+            'name': 'Plant Type',
+            'default': '0.23',
+            'isRequired': False,
+            'notice': '',
+            },
+            ])
+
+        self.poly.onConfig(self.process_config)
+
+    # Process changes to customParameters
+    def process_config(self, config):
+        (valid, changed) = self.params.update_from_polyglot(config)
+        if changed and not valid:
+            LOGGER.debug('-- configuration not yet valid')
+            self.removeNoticesAll()
+            self.params.send_notices(self)
+        elif changed and valid:
+            LOGGER.debug('-- configuration is valid')
+            self.removeNoticesAll()
+            self.configured = True
+            if self.params.isSet('Forecast Days'):
+                self.discover()
+        elif valid:
+            LOGGER.debug('-- configuration not changaed, but is valid')
+
+    def start(self):
+        LOGGER.info('Starting node server')
+        self.set_logging_level()
+        self.check_params()
+        self.discover()
+        LOGGER.info('Node server started')
+        """
+        LOGGER.info('Add node for forecast')
+        for day in range(1,8):
+            address = 'forecast_' + str(day)
+            title = 'Forecast ' + str(day)
+
+            try:
+                node = darksky_daily.DailyNode(self, self.address, address, title)
+                self.addNode(node);
+            except:
+                LOGGER.error('Failed to create forecast node' + title)
+        """
+
+        # Do an initial query to get the data filled in as soon as possible
+        self.query_conditions(True)
+
+    def shortPoll(self):
+        self.query_conditions(False)
+
+    # TODO: Move icon_2_int to a separate file
+    def icon_2_int(self, icn):
+        return {
+                'clear-day': 0,
+                'clear-night': 1,
+                'rain': 2,
+                'snow': 3,
+                'sleet': 4,
+                'wind': 5,
+                'fog': 6,
+                'cloudy': 7,
+                'partly-cloudy-day': 8,
+                'partly-cloudy-night': 9,
+                }.get(icn, 0)
+
+    def get_weather_data(self):
+        request = 'https://api.darksky.net/forecast/'
+        request += self.params.get('APIKey') + '/'
+        request += self.params.get('Location')
+        request += '?units=' + self.params.get('Units')
+
+        #TODO: Can we set the number of days of forecast data?
+
+        LOGGER.debug('request = %s' % request)
+        try:
+            c = requests.get(request)
+            jdata = c.json()
+            c.close()
+            LOGGER.debug(jdata)
+        except:
+            LOGGER.error('HTTP request failed for api.darksky.net')
+            jdata = None
+
+        return jdata
+
+
+    def query_conditions(self, force=False):
+        # Query for the current conditions. We can do this fairly
+        # frequently, probably as often as once every 2 minutes.
+        #
+        # By default JSON is returned
+
+        if not self.configured:
+            LOGGER.info('Skipping connection because we aren\'t configured yet.')
+            return
+
+        try:
+            jdata = get_weather_data()
+
+            if jdata == None:
+                LOGGER.error('Query returned no data')
+                return
+
+            for key in jdata:
+                LOGGER.debug('found key: ' + key)
+                #LOGGER.debug(jdata[key])
+
+            # Assume we always get the main section with data
+            # 'currently' is the current conditions
+            # 'daily' is the daily forecats
+
+            if 'currently' not in jdata:
+                LOGGER.error('No current condition object in query response.')
+                # Note that we're also going to skip forecast data now.
+                return
+
+            ob = jdata['currently']
+            self.update_driver('GV13', self.icon_2_int(ob['icon']), force)
+            self.update_driver('CLITEMP', ob['temperature'], force)
+            self.update_driver('CLIHUM', float(ob['humidity']) * 100, force)
+            self.update_driver('BARPRES', float(ob['pressure']), force)
+            self.update_driver('GV4', float(ob['windSpeed']), force)
+            self.update_driver('GV5', float(ob['windGust']), force)
+            self.update_driver('WINDDIR', float(ob['windBearing']), force)
+            self.update_driver('GV15', float(ob['visibility']), force)
+            self.update_driver('GV14', float(ob['cloudCover'] * 100), force)
+            self.update_driver('GV16', float(ob['uvIndex']), force)
+            self.update_driver('GV0', float(ob['apparentTemperature']), force)
+            self.update_driver('DEWPT', float(ob['dewPoint']), force)
+            self.update_driver('GV6', float(ob['precipIntensity']), force)
+            self.update_driver('GV17', float(ob['ozone']), force)
+            self.update_driver('RAINRT', float(ob['precipIntensity']), force)
+            self.setupdate_driver('GV18', float(ob['precipProbability']) * 100, force)
+
+            # other possible data
+            # nearestStormDistance
+            # precipIntensityError
+            # precipType
+
+            # Daily data is 7 day forecast, index 0 is today
+            for day in range(1,8):
+                address = 'forecast_' + str(day)
+                try:
+                    self.nodes[address].update_forecast(jdata['daily']['data'][day], jdata['latitude'], self.elevation, self.plant_type, self.units)
+                except:
+                    LOGGER.debug('Failed to query forecast data for day ' + day)
+        except:
+            LOGGER.error('Failed to process data from DarkSky.')
+        
+    def query(self):
+        for node in self.nodes:
+            self.nodes[node].reportDrivers()
+
+    def discover(self, *args, **kwargs):
+        # Create forecast nodes here.  We have up to 7 days.
+        LOGGER.info("In Discovery...")
+        num_days = int(slef.params.get('Forecast Days'))
+
+        if num_days < 7:
+            for day in range(num_days + 1, 7):
+                address = 'forecast_' + str(day)
+                try:
+                    self.delNode(address)
+                except:
+                    LOGGER.debug('Failed to delete node ' + address)
+
+        if num_days == 0:
+            return
+
+        for day in range(1, num_days + 1):
+            address = 'forecast_' + str(day)
+            title = 'Forecast ' + str(day)
+            try:
+                node = darksky_daily.DailyNode(self, self.address, address, title)
+                self.addNode(node);
+            except:
+                LOGGER.error('Failed to create forecast node' + title)
+
+        self.set_driver_uom(self.params.get('Units'))
+
+    # Delete the node server from Polyglot
+    def delete(self):
+        LOGGER.info('Removing node server')
+
+    def stop(self):
+        LOGGER.info('Stopping node server')
+
+    def update_profile(self, command):
+        st = self.poly.installprofile()
+        return st
+
+    def check_params(self):
+        self.removeNoticesAll()
+
+        if self.params.get_from_polyglot(self):
+            LOGGER.debug('All required parameters are set!')
+            self.configured = True
+            if int(self.params.get('Forecast Days')) > 7:
+                addNotice('Number of days of forecast data is limited to 7 days', 'forecast')
+                self.params.set('Forecast Days', 7)
+        else:
+            LOGGER.debug('Configuration required.')
+            LOGGER.debug('APIKey = ' + self.params.get('APIKey'))
+            LOGGER.debug('Location = ' + self.params.get('Location'))
+            self.params.set_notices(self)
+
+    def set_driver_uom(self, units):
+        LOGGER.info('Configure driver units to ' + units)
+        self.uom = uom.get_uom(units)
+        for day in range(1, int(self.params.get('Forecast Days')) + 1):
+            address = 'forecast_' + str(day)
+            self.nodes[address].set_driver_uom(units)
+
+    def remove_notices_all(self, command):
+        self.removeNoticesAll()
+
+    def set_logging_level(self, level=None):
+        if level is None:
+            try:
+                level = self.get_saved_log_level()
+            except:
+                LOGGER.error('set_logging_level: get saved log level failed.')
+
+            if level is None:
+                level = 30
+
+            level = int(level)
+        else:
+            level = int(level['value'])
+
+        self.save_log_level(level)
+        LOGGER.info('set_logging_level: Setting log level to %d' % level)
+        LOGGER.setLevel(level)
+
+    commands = {
+            'DISCOVER': discover,
+            'UPDATE_PROFILE': update_profile,
+            'REMOVE_NOTICES_ALL': remove_notices_all,
+            'DEBUG': set_logging_level,
+            }
+
+    # For this node server, all of the info is available in the single
+    # controller node.
+    drivers = [
+            {'driver': 'ST', 'value': 1, 'uom': 2},   # node server status
+            {'driver': 'CLITEMP', 'value': 0, 'uom': 4},   # temperature
+            {'driver': 'GV0', 'value': 0, 'uom': 4},       # apparent temp
+            {'driver': 'CLIHUM', 'value': 0, 'uom': 22},   # humidity
+            {'driver': 'DEWPT', 'value': 0, 'uom': 4},     # dewpoint
+            {'driver': 'BARPRES', 'value': 0, 'uom': 117}, # pressure
+            {'driver': 'GV4', 'value': 0, 'uom': 49},      # wind speed
+            {'driver': 'WINDDIR', 'value': 0, 'uom': 76},  # direction
+            {'driver': 'GV5', 'value': 0, 'uom': 49},      # wind gust
+            {'driver': 'GV13', 'value': 0, 'uom': 25},     # climate conditions
+            {'driver': 'GV14', 'value': 0, 'uom': 22},     # cloud conditions
+            {'driver': 'GV15', 'value': 0, 'uom': 116},    # visibility
+            {'driver': 'GV18', 'value': 0, 'uom': 22},     # chance
+            {'driver': 'RAINRT', 'value': 0, 'uom': 24},   # rain
+            {'driver': 'GV16', 'value': 0, 'uom': 71},     # UV index
+            {'driver': 'GV10', 'value': 0, 'uom': 56},     # Ozone
+            ]
+
--- nodes/darksky_daily.py
+++ nodes/darksky_daily.py
@@ -1,19 +1,19 @@
 # Node definition for a daily forecast node
 
-CLOUD = False
 try:
     import polyinterface
 except ImportError:
     import pgc_interface as polyinterface
-    CLOUD = True
-
 import json
 import time
 import datetime
-import et3
+from nodes import et3
+from nodes import uom
+import node_funcs
 
 LOGGER = polyinterface.LOGGER
 
+@node_funcs.add_functions_as_methods(node_funcs.functions)
 class DailyNode(polyinterface.Node):
     id = 'daily'
     drivers = [
@@ -29,31 +29,9 @@ class DailyNode(polyinterface.Node):
             {'driver': 'GV20', 'value': 0, 'uom': 106},    # mm/day
             ]
 
-    def set_units(self, units):
-        try:
-            for driver in self.drivers:
-                if units == 'us':
-                    if driver['driver'] == 'BARPRES': driver['uom'] = 117
-                    if driver['driver'] == 'GV1': driver['uom'] = 17
-                    if driver['driver'] == 'GV2': driver['uom'] = 17
-                    if driver['driver'] == 'GV19': driver['uom'] = 25
-                elif units == 'si':
-                    if driver['driver'] == 'BARPRES': driver['uom'] = 118
-                    if driver['driver'] == 'GV1': driver['uom'] = 4
-                    if driver['driver'] == 'GV2': driver['uom'] = 4
-                    if driver['driver'] == 'GV19': driver['uom'] = 25
-        except:
-            for drv in self.drivers:
-                if units == 'us':
-                    if drv == 'BARPRES': self.drivers[drv]['uom'] = 117
-                    if drv == 'GV1': self.drivers[drv]['uom'] = 17
-                    if drv == 'GV2': self.drivers[drv]['uom'] = 17
-                    if drv == 'GV19': self.drivers[drv]['uom'] = 25
-                elif units == 'si':
-                    if drv == 'BARPRES': self.drivers[drv]['uom'] = 118
-                    if drv == 'GV1': self.drivers[drv]['uom'] = 4
-                    if drv == 'GV2': self.drivers[drv]['uom'] = 4
-                    if drv == 'GV19': self.drivers[drv]['uom'] = 25
+    def set_driver_uom(self, units):
+        self.uom = uom.get_uom(units)
+        self.units = units
 
     def icon_2_int(self, icn):
         return {
@@ -76,15 +54,15 @@ class DailyNode(polyinterface.Node):
         epoch = int(jdata['time'])
         dow = time.strftime("%w", time.gmtime(epoch))
         LOGGER.info('Day of week = ' + dow)
-        self.setDriver('CLIHUM', round(float(jdata['humidity']) * 100, 0), True, False)
-        self.setDriver('BARPRES', float(jdata['pressure']), True, False)
-        self.setDriver('GV1', float(jdata['temperatureMax']), True, False)
-        self.setDriver('GV2', float(jdata['temperatureMin']), True, False)
-        self.setDriver('GV13', self.icon_2_int(jdata['icon']), True, False)
-        self.setDriver('GV14', round(float(jdata['cloudCover']) * 100, 0), True, False)
-        self.setDriver('GV16', float(jdata['uvIndex']), True, False)
-        self.setDriver('GV18', float(jdata['precipProbability']) * 100, True, False)
-        self.setDriver('GV19', int(dow), True, False)
+        self.update_driver('CLIHUM', round(float(jdata['humidity']) * 100, 0))
+        self.update_driver('BARPRES', float(jdata['pressure']))
+        self.update_driver('GV1', float(jdata['temperatureMax']))
+        self.update_driver('GV2', float(jdata['temperatureMin']))
+        self.update_driver('GV13', self.icon_2_int(jdata['icon']))
+        self.update_driver('GV14', round(float(jdata['cloudCover']) * 100, 0))
+        self.update_driver('GV16', float(jdata['uvIndex']))
+        self.update_driver('GV18', float(jdata['precipProbability']) * 100)
+        self.update_driver('GV19', int(dow))
 
         # Calculate ETo
         #  Temp is in degree C and windspeed is in m/s, we may need to
@@ -102,7 +80,7 @@ class DailyNode(polyinterface.Node):
             Ws = et3.mph2ms(Ws)
 
         et0 = et3.evapotranspriation(Tmax, Tmin, None, Ws, float(elevation), Hmax, Hmin, latitude, float(plant_type), J)
-        self.setDriver('GV20', round(et0, 2), True, False)
+        self.update_driver('GV20', round(et0, 2))
         LOGGER.info("ETo = %f %f" % (et0, self.mm2inch(et0)))
 
 
